9dca222a4f0c3708dba5fb90fcb426c4210d6344
tusharhow <tusharsoftdev@gmail.com>
2022-06-04 08:34:05 -0700

    Faqs content added

--- lib/screens/faq_screens/where_sell_bloks.dart
+++ lib/screens/faq_screens/where_sell_bloks.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
-
-
 class WhereSellBloks extends StatelessWidget {
   const WhereSellBloks({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Where Sell Bloks'),
-      ),
-    
-    
-    );
+        appBar: AppBar(
+          title: const Text(
+            'BLOK larımı nerede satabilirim ?',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 1,
+          iconTheme: const IconThemeData(color: Colors.black),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                'BLOK larınızı özel olarak tasarladığımız MarketPlace (Pazar Alanı) üzerinden satışa koyabilirsiniz.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
